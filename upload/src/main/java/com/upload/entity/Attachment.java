edf1bf519aeda5aab3e63bb302337e3f4c5df2e4
Rivaldo Souza <rivaldo.lsouza@outlook.com>
2022-11-25 14:32:13 -0300

    feat(release/22.0.0): [UPLOAD-2022-11-25] creating image download api endpoint

---
+++ upload/src/main/java/com/upload/entity/Attachment.java
@@ -0,0 +1,31 @@
+package com.upload.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Data@NoArgsConstructor
+@Table(name = "attachment")
+public class Attachment {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String fileName;
+
+    private String fileType;
+
+    @Lob
+    private byte[] data;
+
+    public Attachment(String fileName, String fileType, byte[] data) {
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.data = data;
+    }
+}
---
+++ upload/src/main/java/com/upload/service/AttachmentService.java
@@ -0,0 +1,10 @@
+package com.upload.service;
+
+import com.upload.entity.Attachment;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AttachmentService {
+    Attachment saveAttachment(MultipartFile file) throws Exception;
+
+    Attachment getAttachment(String fileId) throws Exception;
+}
